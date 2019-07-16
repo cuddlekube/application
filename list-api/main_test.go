acdfd753816169349de40bb02816450c9d54b607
Prateek Nayak <6571322+prateeknayak@users.noreply.github.com>
2019-07-16 20:05:58 +1000

    Integrates dynamodb locally to list-api #1

--- list-api/main_test.go
+++ list-api/main_test.go
@@ -1,33 +1,15 @@
 package main
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestRooHandler(t *testing.T) {
-
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(root)
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	expected := `{"message":"Hello World!"}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
-}
 
 func TestVersionHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/version", nil)
@@ -36,7 +18,7 @@ func TestVersionHandler(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(version)
+	handler := http.HandlerFunc(health)
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
@@ -45,6 +27,40 @@ func TestVersionHandler(t *testing.T) {
 	}
 
 	expected := `{"list-api":[{"version":"1.0.0","lastcommitsha":"a1b2c3def"}]}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+// this is a very very brittle test
+// in time need to fix this but it is ok for now
+func TestRegisterHandler(t *testing.T) {
+
+	config := &aws.Config{
+		Region:      aws.String("ap-southeast-2"),
+		Endpoint:    aws.String("http://localhost:8000"),
+		Credentials: credentials.NewStaticCredentials("123", "123", ""),
+	}
+
+	sess := session.Must(session.NewSession(config))
+	dynamo = dynamodb.New(sess)
+
+	req, err := http.NewRequest("GEt", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(list)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"0":{"ckid":"ck1","name":"foo foo cuddly poops"}}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
